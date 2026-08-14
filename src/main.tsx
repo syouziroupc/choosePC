b89e4f4dda4120178d61eb78b72f05516b17efe4
syouziroupc <syouziroupc@gmail.com>
2026-08-14 13:26:54 +0900

    ui: apply final diagnostic polish

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import { installCatalogSuggest } from "./catalogSuggest";
 import "./styles.css";
 import "./catalog-suggest.css";
+import "./diagnostic-polish.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 void installCatalogSuggest();
