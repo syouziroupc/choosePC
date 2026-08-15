6900fa556c3a669db5dcf3842a866ac7af1a0bd2
syouziroupc <syouziroupc@gmail.com>
2026-08-15 13:50:36 +0900

    Use consolidated interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { installCatalogSuggest } from "./catalogSuggest";
 import "./styles.css";
-import "./catalog-suggest.css";
+import "./interaction.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 void installCatalogSuggest();
