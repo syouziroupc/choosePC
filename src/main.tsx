0af8b65a8cf9bafc251ac7f0fe424666e944170f
syouziroupc <syouziroupc@gmail.com>
2026-08-15 13:41:41 +0900

    Unify choosePC styling entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import App from "./App";
 import { installCatalogSuggest } from "./catalogSuggest";
 import "./styles.css";
 import "./catalog-suggest.css";
-import "./diagnostic-polish.css";
-import "./reference-layout.css";
 
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
 void installCatalogSuggest();
